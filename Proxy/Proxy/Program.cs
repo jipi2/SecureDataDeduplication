b9e57b8b0cb09f857ce7d7350a3c4f45bbd6a73f
Jipianu Mihnea <jipianu_mihnea@yahoo.com>
2023-12-28 09:06:03 +0200

    python update

---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,45 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//http client
+builder.Services.AddHttpClient();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//CORS
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.AllowAnyOrigin()
+                                 .AllowAnyHeader()
+                                 .AllowAnyMethod();
+                      });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//CORS
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Proxy/Proxy/UserInt/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace Proxy.UserInt
+{
+    public class Response
+    {
+        public bool Succes { get; set; }
+        public string? Message { get; set; }
+        public string? AccessToken { get; set; }
+    }
+}
